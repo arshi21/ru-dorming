670b6567d7cb96a9472148d6438fb84044f3d214
arshi21 <45611856+arshi21@users.noreply.github.com>
2018-12-17 17:07:43 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -40,3 +40,23 @@ if (module.parent) {
         console.log("server start!")
     })
 }
+
+module.exports = {
+    port: 3000,
+    session: {
+        secret: 'myblog',
+        key: 'myblog',
+        maxAge: 2592000000
+    },
+    db: {
+        host: 'localhost',
+        port: 3306,
+        username: 'root',
+        password: '$Aa12345678',
+        database: 'wx'
+    },
+    admin: {
+        name: 'admin',
+        password: '123'
+    }
+}
